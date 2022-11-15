ced207eaaecb93414644431fe89e9f88890462c1
mateo piccolomini <matepicco19@gmail.com>
2022-11-15 10:00:10 -0300

    testeo Lectura y Suma (explicacion)

--- libreria/MiLibreria.cpp
+++ libreria/MiLibreria.cpp
@@ -14,6 +14,7 @@ enum obraSocial
 struct fecha
 {
 	unsigned int dia, mes, anio;
+
 }; typedef struct fecha fecha;
 
 struct contacto
@@ -23,6 +24,7 @@ struct contacto
 	string email;
 	string telefono; //(+54)911-4444-5678
 	string celular;
+
 }; typedef struct contacto Cont;
 
 struct consulta
@@ -33,7 +35,8 @@ struct consulta
 	string ensurance1, ensurance2; // por las dudas
 	string matriculaMed;
 	bool attendance;
-}; typedef struct consulta Cons;
+
+}; typedef struct consulta Cons;	
 
 struct medico
 {
@@ -42,6 +45,7 @@ struct medico
 	string telefono;
 	string especialidad;
 	bool activo;
+
 }; typedef struct medico Med;
 
 struct paciente
@@ -54,6 +58,7 @@ struct paciente
 	int ensurance;
 	contacto Cont;//variable contacto de tipo contacto
 	consulta Cons; //variable consulta de tipo consulta
+
 }; typedef struct paciente Pac;
 
 //invocacion funciones
@@ -62,6 +67,9 @@ bool EscrituraCsv(string NombreArchi, Pac*& l_Pacientes, int* tamactual);
 bool agregar(Pac*& l_Pacientes, Pac paciente, int* tamactual);
 bool resize(Pac*& l_Pacientes, int* tamactual, int cantidad_aumentar);
 bool Secretaría(Pac*& l_Pacientes, int* tamactual, unsigned int dni);
+//PROTOTIPOS
+bool ProtoLectura(string PacientesA, string ConsultasA, string ContactoA, int dni);
+int suma(int num, int num2);
 
 Pac* LecturaCsv(string PacientesA, string ConsultasA, string ContactoA, int dni)
 {
@@ -84,7 +92,7 @@ Pac* LecturaCsv(string PacientesA, string ConsultasA, string ContactoA, int dni)
 	char coma;
 	int tamact = 0;
 
-	//leo headers todos archivos (3) - no se leen los ENDL cuando leyendo headers
+	//leo headers todos archivos (3) ARREGLAR- no se leen los ENDL cuando leyendo headers
 	fp >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >>
 		dummy >> coma >> dummy >> coma >> dummy;
 	fp2 >> dummy >> coma >> dummy >> coma >> dummy;
@@ -125,6 +133,75 @@ Pac* LecturaCsv(string PacientesA, string ConsultasA, string ContactoA, int dni)
 	return l_Pacientes;
 }
 
+//LECTURA PROTOTIPO UNITTEST
+bool ProtoLectura(string PacientesA, string ConsultasA, string ContactoA, int dni)
+{
+	fstream fp;
+	fstream fp2;
+	fstream fp3;
+	fp.open(PacientesA, ios::in);
+	fp2.open(ConsultasA, ios::in);
+	fp3.open(ContactoA, ios::in);
+
+	if (!((fp.is_open()) && (fp2.is_open()) && (fp3.is_open())))
+		return false;
+
+	Pac* l_Pacientes = new Pac[0]; //deberia ser tipo puntero, pero así, no me toma error
+	Pac aux1;
+	consulta aux2;
+	contacto aux3;
+	// nota naux;//a q equivale en nuestro cod
+	string dummy;
+	char coma;
+	int tamact = 0;
+
+	//leo headers todos archivos (3) ARREGLAR- no se leen los ENDL cuando leyendo headers
+	fp >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >>
+		dummy >> coma >> dummy >> coma >> dummy;
+	fp2 >> dummy >> coma >> dummy >> coma >> dummy;
+	fp3 >> dummy >> coma >> dummy >> coma >> dummy;
+
+	while (fp)
+	{
+		fp >> aux1.DNI >> coma >> aux1.firstName >> coma >> aux1.lastName >> coma >> aux1.gender >> coma >>
+			aux1.birthDate.dia >> coma >> aux1.birthDate.mes >> coma >> aux1.birthDate.anio >> coma >> aux1.VitalState;
+		//recorre, lee archivo paciente y guarda en struct paciente
+		while ((dni == aux1.DNI) || fp2) {
+			fp2 >> dni >> coma; //cambiar todos headers de----------------------------------------------
+			if (dni == aux2.DNI) {
+				l_Pacientes->Cons = aux2;
+				break; //no necesario? condicion while
+			}
+		}
+		while ((dni == aux3.DNI) || fp3)
+		{
+			fp3 >> dni >> coma; //cambiar todos headers de------------------------------------------------
+			if (dni == aux3.DNI) {
+				l_Pacientes->Cont = aux3;
+				break; //no necesario? condicion while
+			}
+		}
+		fp2.seekg(fp2.beg);//sale del while fp2? vuelve al principio
+		fp3.seekg(fp3.beg);
+		// Volvemos a salter el encabezado de fp2, porque posicionamos el cursor de lectura al inicio del archivo.
+
+		agregar(l_Pacientes, aux1, &tamact);
+		//srgun procedimiento l_pacientes deberia llevar una lista tipo pointer, pero al no permitirnos, lleva tipo struct
+	}
+
+	fp.close();
+	fp2.close();
+	fp3.close();
+
+	return true;
+}
+
+//PROTOTIPO UNIT TEST
+int suma( int num, int num2)
+{
+	//no me deja configurarla en el main: muchos valores inicializados?(2)
+	return num + num2;
+}
 bool resize(Pac*& l_Pacientes, int* tamactual, int cantidad_aumentar) {
 
 	if (!(l_Pacientes == nullptr) || (tamactual == nullptr))
@@ -133,15 +210,15 @@ bool resize(Pac*& l_Pacientes, int* tamactual, int cantidad_aumentar) {
 
 	*tamactual = *tamactual + cantidad_aumentar;
 	int i = 0;
-	Pac* NewListPac = new Pac[*tamactual];
+	Pac* NewListResi = new Pac[*tamactual];
 	while (i < *tamactual - cantidad_aumentar) 
 	{
-		NewListPac[i] = l_Pacientes[i];
+		NewListResi[i] = l_Pacientes[i];
 		i++;
 	}
 
 	delete[] l_Pacientes;
-	l_Pacientes = NewListPac;
+	l_Pacientes = NewListResi;
 
 	//implementacion de AGREGAR?
 
@@ -165,6 +242,99 @@ bool agregar(Pac*& l_Pacientes, Pac aux1, int* tamactual)
 	delete[] l_Pacientes;
 	l_Pacientes = NewListPac;
 
+	return true;
+}
+
+bool Busqueda(Pac*& l_Pacientes, contacto* l_Contactos, consulta* l_Consultas, int* tamactual, int dni)
+//N es variable entera que viene por funcion como parametro formal
+{
+	int num = 0; //variable para recibir éxito o defecto de funciones de archivos
+	int i;
+
+	bool check = true;
+	Pac* AuxErroneos;//ENVEZ = implementar funcion AGREGAR
+
+	//implementamos utilización LIBRERIA CTIME entonces transformamos variables segundos con time_t.
+	int TenañosEnSeg = 10;
+	time_t now;
+	time_t timer;
+	struct tm TenyAgo = { 0 };
+	double seconds;
+	time(&now);
+
+	TenyAgo.tm_hour = 0;   TenyAgo.tm_min = 0; TenyAgo.tm_sec = 0;
+	TenyAgo.tm_year = TenyAgo.tm_year - 10; TenyAgo.tm_mon = 0; TenyAgo.tm_mday = 1;
+
+	time(&timer);  /* get current time; same as: timer = time(NULL)  */
+
+	seconds = difftime(now, mktime(&TenyAgo));
+
+	Pac* PacAux = new Pac[*tamactual];//?
+	PacAux = l_Pacientes;
+	for (i = 0; i < *tamactual; i++)
+	{
+		//PARA NO LLAMAR A ESCRITURA CONTINUOUSLY. generar lista previa para fallecidos archivados recuperables
+		//EN VEZ DE COPIAR A LISTA ACÁ. GENERAR LISTAS DINAMICAS Y LLAMARA AGREGAR implementandola dandole uso en programa
+
+		//pacientes fallecidos
+		if ((PacAux[i].VitalState == "Fallecido") && (PacAux[i].DNI == dni))
+		{//mismo proceso pero en diferente archivo: "Dead.csv"
+			//a fx escritura le llega por nombre tipo string (dead.csv) y la fx tiene q aplicar a q escriba en ese archivo correctly
+			//(l_Pacientes.VitalState == "fallecido") || (l_Pacientes.VitalState == "Fallecido")
+			// O CONTROLAR con TLOWER en ingreso de data
+			check = EscrituraCsv("Fallecidos.csv", PacAux, tamactual);
+			if (check == false)
+				AuxErroneos[i] = PacAux[i];//muevo el paciente a lista para re-visar los errores
+		}
+		else
+			if ((PacAux[i].VitalState == "Internado") && (PacAux[i].DNI == dni))
+			{//(l_Pacientes.VitalState == "internado") || (l_Pacientes.VitalState == "Internado")
+				// O CONTROLAR con TLOWER en ingreso de data
+				check = EscrituraCsv("Archivados.csv", PacAux, tamactual);
+				if (check == false)
+					AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
+			}
+			else
+				//posibles recuperables
+				if (PacAux[i].VitalState == "n/c" && (PacAux[i].DNI == dni)) //Paciente desconocida vitalidad = potrencial recuperable
+				{
+					//NO TIENE SENTIDO ALGUNO MANEJAR DATA, si NO SABES SI VIVEN
+					if ((PacAux[i].Cons.ultConsulta.anio + TenañosEnSeg < now) && (PacAux[i].DNI == dni))
+					{
+						check = EscrituraCsv("Archivados.csv", PacAux, tamactual);
+						if (check == false)
+							AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
+					}
+					else //si paciente SE CONSULTÓ hace MENOS DE 10 AÑOS = RECUPERABLE.cambiar
+						if (PacAux[i].Cons.attendance == true)
+						{		//lama a fxEscritura y escribe en archivo RECUPERABLES
+								//archivo recuperables ya existe (caso contrario, lo crea)
+							check = EscrituraCsv("Recuperables.csv", PacAux, tamactual);
+							if (check == true)
+							{	//llama funcion secretaria en donde la secretaria contacta al paciente 
+								//funcion secretaria recibe un array de tipo Paciente y Consulta para editarlos
+								check = Secretaría(PacAux, tamactual, dni);
+								if (check == false)
+									AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
+								/*en la cual el desarrollo incluiría :
+								* decisión: retorno/no retorna
+								* posible fallo: paciente no responde solucion:volver a contactar en tiempo
+								* posible cambio obra social. caso positivo/negativo: informa y actualiza
+								* OBRA SOCIAL 2 DEBERÍA ESTAR EN BLANK
+								* genera y escribe archivos pertinentes
+								* envia
+								* IGUALAR fxSecretaria a variable NUM
+								* si num == 1, proceso exitoso, si num==0, error
+								*/
+							}
+							else
+								AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
+						}
+				}
+	}
+	delete[]PacAux;
+	l_Pacientes = PacAux; //redirecciono
+	//ERRONEOS se envian todos juntos de una, acá hacia secretaria, SE BORRA MEMORIA LUEGO, debajo de tal instruccion
 	return true;
 }
 
@@ -204,95 +374,7 @@ bool EscrituraCsv(string NombreArchi, Pac*& l_Pacientes, int* tamactual)
 
 }
 
-
 bool Secretaría(Pac*& PacAux, int* tamactual, unsigned int dni)
 {
-	return true;
-}
-
-bool Busqueda(Pac*& l_Pacientes, contacto* l_Contactos, consulta* l_Consultas, int* tamactual, int dni)
-//N es variable entera que viene por funcion como parametro formal
-{
-	int num = 0; //variable para recibir éxito o defecto de funciones de archivos
-	int i;
-
-	bool check = true;
-	Pac* AuxErroneos;//ENVEZ = implementar funcion AGREGAR
-
-	//implementamos utilización LIBRERIA CTIME entonces transformamos variables segundos con time_t.
-	int TenañosEnSeg = 10;
-	time_t now;
-	time_t timer;
-	struct tm TenyAgo = { 0 };
-	double seconds;
-	time(&now);
-
-	TenyAgo.tm_hour = 0;   TenyAgo.tm_min = 0; TenyAgo.tm_sec = 0;
-	TenyAgo.tm_year = TenyAgo.tm_year - 10; TenyAgo.tm_mon = 0; TenyAgo.tm_mday = 1;
-
-	time(&timer);  /* get current time; same as: timer = time(NULL)  */
-
-	seconds = difftime(now, mktime(&TenyAgo));
-
-	Pac* PacAux = new Pac[*tamactual];//?
-	PacAux = l_Pacientes;
-	for (i = 0; i < *tamactual; i++)
-	{
-		//pacientes fallecidos
-		if ((PacAux[i].VitalState == "Fallecido") && (PacAux[i].DNI == dni))
-		{//mismo proceso pero en diferente archivo: "Dead.csv"
-			//a fx escritura le llega por nombre tipo string (dead.csv) y la fx tiene q aplicar a q escriba en ese archivo correctly
-			//(l_Pacientes.VitalState == "fallecido") || (l_Pacientes.VitalState == "Fallecido")
-			// O CONTROLAR con TLOWER en ingreso de data
-			check = EscrituraCsv("Fallecidos.csv", PacAux, tamactual);
-			if (check == false)
-				AuxErroneos[i] = PacAux[i];//muevo el paciente a lista para re-visar los errores
-		}
-		else
-			if ((PacAux[i].VitalState == "Internado") && (PacAux[i].DNI == dni))
-			{//(l_Pacientes.VitalState == "internado") || (l_Pacientes.VitalState == "Internado")
-				// O CONTROLAR con TLOWER en ingreso de data
-				check = EscrituraCsv("Archivados.csv", PacAux, tamactual);
-				if (check == false)
-					AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
-			}
-			else
-				//posibles recuperables
-				if (PacAux[i].VitalState == "n/c" && (PacAux[i].DNI == dni)) //Paciente desconocida vitalidad = potrencial recuperable
-				{
-					//NO TIENE SENTIDO ALGUNO MANEJAR DATA, si NO SABES SI VIVEN
-					if ((PacAux[i].Cons.ultConsulta.anio + TenañosEnSeg < now) && (PacAux[i].DNI == dni))
-					{
-						check = EscrituraCsv("Archivados.csv", PacAux, tamactual);
-						if (check == false)
-							AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
-					}
-				}
-				else
-					if (PacAux[i].Cons.attendance == true)
-					{		//lama a fxEscritura y escribe en archivo RECUPERABLES
-							//archivo recuperables ya existe (caso contrario, lo crea)
-						check = EscrituraCsv("Recuperables.csv", PacAux, tamactual);
-						if (check == true)
-						{	//llama funcion secretaria en donde la secretaria contacta al paciente 
-							//funcion secretaria recibe un array de tipo Paciente y Consulta para editarlos
-							check = Secretaría(PacAux, tamactual, dni);
-							if (check == false)
-								AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
-							/*en la cual el desarrollo incluiría :
-							* decisión: retorno/no retorna
-							* posible fallo: paciente no responde solucion:volver a contactar en tiempo
-							* posible cambio obra social. caso positivo/negativo: informa y actualiza
-							* OBRA SOCIAL 2 DEBERÍA ESTAR EN BLANK
-							* genera y escribe archivos pertinentes
-							* envia
-							* IGUALAR fxSecretaria a variable NUM
-							* si num == 1, proceso exitoso, si num==0, error
-							*/
-						}
-						else
-							AuxErroneos[i].DNI = PacAux[i].DNI;//muevo dni del paciente a lista para re-visar los errores de escritura
-					}
-	}
 	return true;
 }
--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -1,15 +1,52 @@
 #include "gmock/gmock.h"
+#include "MiLibreria.cpp"
 
-namespace Casos_Base::tests {
-	TEST(Casos_Base, Test_Caso1) {
-		ASSERT_THAT(1, 1);
+namespace Casos_Base::tests
+{
+	TEST(LecturaCsv, lecturaCorrecta) { //FUNCIONA TEST +
+		//AAA --> Arrange-Act-Assert
+		//Arrange
+		//generar string randoms para nombre?
+		string PacientesA = "hola.?";
+		string ConsultasA = "<210ds´+";
+		string ContactosA = "sidndan";
+		
+		int DNI = 45296137;
+
+		//Act
+		bool result = true;
+		result = ProtoLectura(PacientesA, ConsultasA, ContactosA, DNI);
+
+		//Assert
+		EXPECT_EQ(ProtoLectura("hola.?", "<210ds´+", "sidndan", 45296137), result);
 	}
 
+	TEST(LecturaCsv, lecturaINCorrecta)
+	{
+		//AAA --> Arrange-Act-Assert
+		//Arrange
+		string PacientesA = "";
+		string ConsultasA = "/0" ;
+		string ContactosA;
+	}
 	TEST(Casos_Base, Test_Caso2) {
 		ASSERT_THAT(1, 1);
 	}
 
 	TEST(Casos_Base, Test_Caso3) {
 		ASSERT_THAT(1, 1);
+	}
+
+	TEST(explicacion, suma) {
+		//AAA --> Arrange-Act-Assert
+		//Arrange
+		int a = 4;
+		int b = 4;
+		int expected = 8;
+		
+		//Act
+		int actual = suma(a, b);
+
+		ASSERT_EQ(expected, actual);
 	}
 }
