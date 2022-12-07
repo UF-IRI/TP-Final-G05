e71e73d56b62b3d42664d56153dff38477181e13
mateo piccolomini <matepicco19@gmail.com>
2022-12-06 23:37:44 -0300

    TODO OKEY!

--- unit-test/main_test.cpp
+++ unit-test/main_test.cpp
@@ -115,5 +115,47 @@ namespace foobar::tests
 		EXPECT_NE(expected, actual);
 		EXPECT_FALSE(actual);
 	}
+	TEST(Busqueda, NoNulo)
+	{
+		string pathPac, pathCons, pathCont, pathMed;
+
+		pathPac = "C:\\Users\\54232\\Desktop\\Mateo\\2022\\BMI\\IRI\\TRABAJO FINAL\\data_files\\input\\Pacientes.csv";
+		pathCons = "C:\\Users\\54232\\Desktop\\Mateo\\2022\\BMI\\IRI\\TRABAJO FINAL\\data_files\\input\\Consultas.csv";
+		pathCont = "C:\\Users\\54232\\Desktop\\Mateo\\2022\\BMI\\IRI\\TRABAJO FINAL\\data_files\\input\\Contactos.csv";
+		pathMed = "C:\\Users\\54232\\Desktop\\Mateo\\2022\\BMI\\IRI\\TRABAJO FINAL\\data_files\\input\\Medicos.csv";
+
+		fstream fp;
+		fstream fp2;
+		fstream fp3;
+		fstream fp4;
+
+		fp.open(pathPac, ios::in);
+		fp2.open(pathCont, ios::in);
+		fp3.open(pathCons, ios::in);
+		fp4.open(pathMed, ios::in);
+
+		if (!(fp.is_open()))
+			return;
+		if (!(fp2.is_open()))
+			return;
+		if (!(fp3.is_open()))
+			return;
+		if (!(fp4.is_open()))
+			return;
+
+		int n = 0;
+		Pac* actual = new Pac[10];
+		actual = Lectura(fp, fp2, fp3, fp4, n);
+		
+		bool expected = true;
+		bool actualBus = Busqueda(actual, &n, 10242582);
+
+		ASSERT_THAT(expected, actual);
+
+		string dato = actual[0].Cons.ultConsulta;
+		string dato2= actual[1].Cons.ultConsulta;
+
+		EXPECT_NE(dato, dato2);
+	}
 
 }
